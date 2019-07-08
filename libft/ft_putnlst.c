7d3f750d4dc3a29f9fc300e6905c8f803f72b36b
Vaughan SCOTT <vscott@c5r3s4.wethinkcode.co.za>
2019-07-08 17:16:15 +0200

    nul 4 putlst

--- libft/ft_putnlst.c
+++ libft/ft_putnlst.c
@@ -6,7 +6,7 @@
 /*   By: vscott <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/07 15:25:05 by vscott            #+#    #+#             */
-/*   Updated: 2019/07/07 16:03:10 by vscott           ###   ########.fr       */
+/*   Updated: 2019/07/08 12:40:50 by vscott           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 void		ft_putnlst(t_list *lst)
 {
+	if (!lst)
+		ft_putendl("(null)");
 	while (lst)
 	{
 		ft_putnbr(*((int*)(lst->content)));
--- libft/ft_putslst.c
+++ libft/ft_putslst.c
@@ -6,7 +6,7 @@
 /*   By: vscott <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/07 15:21:09 by vscott            #+#    #+#             */
-/*   Updated: 2019/07/07 15:56:02 by vscott           ###   ########.fr       */
+/*   Updated: 2019/07/08 12:40:09 by vscott           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 void		ft_putslst(t_list *lst)
 {
+	if (!lst)
+		ft_putendl("(null)");
 	while (lst)
 	{
 		ft_putendl(lst->content);
