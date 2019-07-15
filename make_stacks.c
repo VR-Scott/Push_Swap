3fe5c2f4ef72c1b54f9d9ffae2d5c48dd60ef27b
Vaughan SCOTT <vscott@c5r3s4.wethinkcode.co.za>
2019-07-15 17:33:15 +0200

    mod push_swap.h,  add : push_swap.c, make_stacks.c

--- make_stacks.c
+++ make_stacks.c
@@ -1,37 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
+/*   make_stacks.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vscott <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/07/15 16:30:21 by vscott            #+#    #+#             */
-/*   Updated: 2019/07/15 17:31:35 by vscott           ###   ########.fr       */
+/*   Created: 2019/07/15 17:11:06 by vscott            #+#    #+#             */
+/*   Updated: 2019/07/15 17:31:30 by vscott           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PUSH_SWAP_H
-# define PUSH_SWAP_H
+#include "push_swap.h"
 
-# include "libft.h"
-#define MAX_INT 2147483647
-#define MIN_INT -2147483648
-
-typedef struct	s_stacks
-{
-	int			*stk_a;
-	int			*stk_b;
-	int			*a_sz;
-	int			*b_sz;
-}				t_stacks;
-
-typedef struct	s_moves
-{
-	int			a_mvs;
-	int			b_mvs;
-	int			c_mvs;
-	char		*a_rt;
-	char		*b_rt;
-	char		*c_rt;
-	int			tot;
-}				s_moves;
+void	make_stacks(t_stacks *stacks, char * )
--- push_swap.c
+++ push_swap.c
@@ -1,37 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vscott <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/07/15 16:30:21 by vscott            #+#    #+#             */
-/*   Updated: 2019/07/15 17:31:35 by vscott           ###   ########.fr       */
+/*   Created: 2019/07/15 16:52:13 by vscott            #+#    #+#             */
+/*   Updated: 2019/07/15 17:31:32 by vscott           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PUSH_SWAP_H
-# define PUSH_SWAP_H
+#include "push_swap.h"
 
-# include "libft.h"
-#define MAX_INT 2147483647
-#define MIN_INT -2147483648
-
-typedef struct	s_stacks
+int	main(int ac, char **av)
 {
-	int			*stk_a;
-	int			*stk_b;
-	int			*a_sz;
-	int			*b_sz;
-}				t_stacks;
+	t_stacks	*stacks;
 
-typedef struct	s_moves
-{
-	int			a_mvs;
-	int			b_mvs;
-	int			c_mvs;
-	char		*a_rt;
-	char		*b_rt;
-	char		*c_rt;
-	int			tot;
-}				s_moves;
+	if (ac > 1)
+		{
+			stacks = (t_stacks*)malloc(sizeof(t_stacks));
+			make_stacks(stacks, av[1]);
+		}
+	return (0);
+}
